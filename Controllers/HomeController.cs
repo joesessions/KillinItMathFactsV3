91960e5337eb41600e15cb6caa733ca16d100719
joesessions <36654608+joesessions@users.noreply.github.com>
2018-07-06 15:43:10 -0700

    Prior pulling data from the view.
    At working point in the data.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,12 +21,42 @@ namespace KillinItMathFactsV3.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult PersonalMFAStore(string performanceMF1, string performanceMF2)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=KillinItMathFacts;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            
+            string queryString = "INSERT INTO personalMFA (performanceMF1, performanceMF2) VALUES (@performanceMF1, @performanceMF2)";
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@performanceMF1", SqlDbType.Decimal);
+                command.Parameters.Add("@performanceMF2", SqlDbType.Decimal);
+                command.Parameters["@performanceMF1"].Value = performanceMF1;
+                command.Parameters["@performanceMF2"].Value = performanceMF2;
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                
+            }
+            
+                return null;
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        public ActionResult TestLand()
+        {
+
+            return View();
+        }
+
     }
 }
+
+
+
